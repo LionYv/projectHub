5afb9b87b7bfab6dc5cbc53888ed292cbdfbc917
LionYv <80548380+LionYv@users.noreply.github.com>
2023-03-10 18:31:56 +0200

    color chooser & slight modifications

--- catan/SettleComponent.java
+++ catan/SettleComponent.java
@@ -14,14 +14,15 @@ public class SettleComponent extends JPanel {
 
     @Override
     public void paintComponent(Graphics g) {
-        //super.paintComponent(g);
         g.setColor(c);
+        // a new settlement
         if (level == 0) {
             g.fillOval(0, 0, 23, 23);
             g.drawOval(10, 10, 23, 23);
         }
         else
         {
+            // a city
             g.fillRect(-5, -10, 30, 30);
             g.drawRect(-5, -10, 30, 30);
         }
