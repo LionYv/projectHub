fcbac07e3cc293b71372436b737f2edcea32db0c
LionYv <80548380+LionYv@users.noreply.github.com>
2023-03-10 18:50:03 +0200

    color chooser & slight modifications

--- catan/MyPanel.java
+++ catan/MyPanel.java
@@ -14,6 +14,7 @@ public class MyPanel extends JLayeredPane implements ActionListener {
     public static int settlementsPlaced = 0;
     public static Player[] players;
     public static final int BOARD_HEIGHT = 1500;
+    // used for calculations
     private static final double sqrt3div2 = 0.86602540378;
     public static int heightMargin = 100;
     public static int hexagonSide;
@@ -30,6 +31,7 @@ public class MyPanel extends JLayeredPane implements ActionListener {
 
     MyPanel(GameSetup parent)
     {
+             // setting a new game panel
              this.setLayout(null);
              this.setBounds(0,0,1600,1600);
              this.setBackground(new Color(90,120,90));
@@ -37,11 +39,11 @@ public class MyPanel extends JLayeredPane implements ActionListener {
              players = new Player[2];
              players[0] = new Player(1,new int[6],null);
              players[1] = new Player(2,new int[6], null);
+             // make players choose a color
              players[0].color = JColorChooser.showDialog(new JColorChooser(), "player 0: choose a color", Color.WHITE);
              players[1].color = JColorChooser.showDialog(new JColorChooser(), "player 1: choose a color", Color.WHITE);
-
              frameParent = parent;
-             initTiles();
+             initTiles(); // start painting the board
 
     }
 
@@ -52,12 +54,14 @@ public class MyPanel extends JLayeredPane implements ActionListener {
             tileColors[i] = new Tile(null,0,0,new Point(0,0), 0, false);
 
         }
-            for (int i = 0; i < 19; i++) {
+        for (int i = 0; i < 19; i++)
+        {
                 tileArray[i] = new Tile(null,0,0,new Point(),0, false);
-            }
+        }
     }
     public void addMessageDisplay()
     {
+        // currently a place holder
         JLabel jb = new JLabel("");
         jb.setLayout(null);
         jb.setBounds(330,50,190,40);
@@ -67,60 +71,9 @@ public class MyPanel extends JLayeredPane implements ActionListener {
     {
         frameParent.dispose();
     }
-    public void setRoadArr()
-    {
-         /*roads = new Road[]{
-                 new Road(561, 416, 0,false),
-                 new Road(667, 107, 0,false),
-                 new Road(802, 109, 0,false),
-                 new Road(900, 171, 0,false),
-                 new Road(967, 288, 0,false),
-                 new Road(969, 412, 0,false),
-                 new Road(899, 527, 0,false),
-                 new Road(802, 592, 0,false),
-                 new Road(669, 589, 0,false),
-                 new Road(564, 532, 0,false),
-                 new Road(499, 411, 0,false),
-                 new Road(570, 176, 0,false),
-                 new Road(493, 289, 0,false),
-                 new Road(599, 243, 0,false),
-                 new Road(642, 294, 0,false),
-                 new Road(706, 290, 0,false),
-                 new Road(731, 240, 0,false),
-                 new Road(765, 294, 0,false),
-                 new Road(840, 294, 0,false),
-                 new Road(868, 230, 0,false),
-                 new Road(831, 168, 0,false),
-                 new Road(763, 170, 0,false),
-                 new Road(639, 164, 0,false),
-                 new Road(697, 164, 0,false),
-                 new Road(935, 350, 0,false),
-                 new Road(899, 416, 0,false),
-                 new Road(834, 410, 0,false),
-                 new Road(763, 416, 0,false),
-                 new Road(695, 410, 0,false),
-                 new Road(796, 348, 0,false),
-                 new Road(660, 352, 0,false),
-                 new Road(628, 414, 0,false),
-                 new Road(558, 409, 0,false),
-                 new Road(559, 294, 0,false),
-                 new Road(527, 344, 0,false),
-                 new Road(900, 290, 0,false),
-                 new Road(870, 473, 0,false),
-                 new Road(734, 475, 0,false),
-                 new Road(598, 472, 0,false),
-                 new Road(637, 536, 0,false),
-                 new Road(706, 532, 0,false),
-                 new Road(831, 539, 0,false),
-                 new Road(763, 525, 0,false),
-                 new Road(667, 588, 0,false),
-                 new Road(807, 589, 0,false)
-         };
-
-          */
-    }
+   
     public void addButtons(GameBoard g)
-    {
+    {   // adding functional buttons: roll dice, end turn
         JButton roll = new JButton("roll a dice");
         roll.setBounds(100,100,100,100);
         roll.addMouseListener(new MouseAdapter() {
@@ -147,18 +100,15 @@ public class MyPanel extends JLayeredPane implements ActionListener {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         g2.setStroke(new BasicStroke(6));
-
-         hexagonSide = (BOARD_HEIGHT - 2 * heightMargin) / 18;
-         widthMargin = (getWidth() - (int) (10 * hexagonSide * sqrt3div2)) / 2;
-         drawGrid(g2);
-         generateNums(g2);
-        if(!wasInitialized)
+        hexagonSide = (BOARD_HEIGHT - 2 * heightMargin) / 18;
+        widthMargin = (getWidth() - (int) (10 * hexagonSide * sqrt3div2)) / 2;
+        // paint hexagons and generate numbers for each hexagon
+        drawGrid(g2);
+        generateNums(g2);
+        if(!wasInitialized) // only happens one time
         {
-            createPosForSettle();
-            System.out.println("----------");
-            System.out.println(settlements[0] + " " + settlements[1]);
-            System.out.println("---------");
-            buildRoads();
+            createPosForSettle(); // create the positions in which a settlement will be placed
+            buildRoads(); // initialize road array
 
             GameBoard gameBoard = new GameBoard(tileArray, roads,settlements, players, this);
             MouseListener aMouse = new MouseListen(this, gameBoard);
@@ -168,26 +118,6 @@ public class MyPanel extends JLayeredPane implements ActionListener {
         }
         wasInitialized = true;
     }
-    public void createAvgPosForSettle()
-    {
-        for (int i = 0; i <tileArray.length; i++) {
-            Polygon currP = tileArray[i].getPolygon();
-            for (int j = 0; j < currP.npoints; j++) {
-                int x = currP.xpoints[j];
-                int y = currP.ypoints[j];
-                if (settlementsPlaced < settlements.length)
-                {
-                    settlements[settlementsPlaced++] = new Settlement(x,y,0);
-                    System.out.println(settlements[settlementsPlaced-1].toString());
-                }
-            }
-
-        }
-        for (int i = 0; i < settlementsPlaced; i++) {
-            Point pos = new Point(settlements[i].getX(), settlements[i].getY());
-
-        }
-    }
 
     public void createPosForSettle()
     {
@@ -196,15 +126,13 @@ public class MyPanel extends JLayeredPane implements ActionListener {
             for (int j = 0; j < currP.npoints; j++) {
                 int x = currP.xpoints[j];
                 int y = currP.ypoints[j];
-                if (!AlreadyIn(x,y) && settlementsPlaced < settlements.length)
+                if (!AlreadyIn(x,y) && settlementsPlaced < settlements.length) // if the position isnt in yet
                 {
                     settlements[settlementsPlaced++] = new Settlement(x,y,0);
-                    System.out.println(settlements[settlementsPlaced-1].toString());
                 }
             }
 
         }
-        System.out.println(settlementsPlaced);
 
     }
     public void generateNums(Graphics2D g2)
@@ -219,7 +147,7 @@ public class MyPanel extends JLayeredPane implements ActionListener {
         else {
             nums = new Integer[]{2, 3, 3, 4, 4, 5, 5, 6, 7, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12};
             List<Integer> numList = Arrays.asList(nums);
-            Collections.shuffle(numList);
+            Collections.shuffle(numList); // shuffle array randomly
             numList.toArray(nums);
             System.out.println(Arrays.toString(nums));
             for (int i = 0; i < tileArray.length; i++) {
@@ -239,35 +167,35 @@ public class MyPanel extends JLayeredPane implements ActionListener {
 
 
     public void drawGrid(Graphics2D g2) {
+        // drawing all the hexagon tiles
         int constant = 600;
         int offset = 65;
         // y(x) = 110 + x*115
         int startY = 110;
         int yDiff = 120;
-        ///////////////////////
-        if (wasInitialized) {
+        
+        if (wasInitialized) { // just redraw
             for (int i = 0; i < tileArray.length; i++) {
-                //System.out.println("resource: " + tileArray[i].getResource() + " " + GetColor(tileArray[i].getResource()));
                 drawHex(tileArray[i].getX(), tileArray[i].getY(), g2, (GetColor(tileArray[i].getResource())));
-                // drawString(tile x,y,number,g2)
-
             }
-        } else {
+        }
+        else {
 
             for (int i = 0; i < 5; i++) {
                 tileColors[i].amt = 4;
                 tileColors[i].isFour = true;
             }
             Random rnd = new Random();
+            // decide randomly which element will only have 3 hexagons
             int ran = rnd.nextInt(5);
             tileColors[ran].amt = 3;
             tileColors[ran].isFour = false;
             int x = -1;
             int cnt = 0;
             for (int i = 0; i <= 2; i++) {
-                //if ()
                 Color c = generateTileType(tileColors);
-                x = determineResource(c);
+                x = determineResource(c); // determine resource for current tile, based on color randomly chosen
+                // create the new hexagon tile with matching color
                 tileArray[cnt++] = new Tile(makeHex(constant + i * 135, startY), x, 0, new Point(constant + i * 135, startY), 0, false);
                 drawHex(constant + i * 135, startY, g2, c);
                 c = generateTileType(tileColors);
@@ -293,7 +221,6 @@ public class MyPanel extends JLayeredPane implements ActionListener {
                 drawHex(constant - 270 + i * 135 + offset * 2, startY + yDiff * 2, g2, c);
                 tileArray[cnt++] = new Tile(makeHex(constant - 270 + i * 135 + offset * 2, startY + yDiff * 2), x, 0, new Point(constant - 270 + i * 135 + offset * 2, startY + yDiff * 2), 0, false);
             }
-            printTiles();
         }
     }
 
@@ -302,19 +229,18 @@ public class MyPanel extends JLayeredPane implements ActionListener {
         for (int i = 0; i < tileArray.length; i++) {
             Polygon p = tileArray[i].getPolygon();
             if (p != null) {
-                // System.out.println(Arrays.toString(p.xpoints));
-                for (int k = 0; k < 5; k++) {
+                for (int k = 0; k < 6; k++) { // add each road start and end location to road array
                     roads[placed++] = (new Road(p.xpoints[k]-3, p.ypoints[k]-5,
-                            p.xpoints[k + 1]-3, p.ypoints[k + 1]-5,null));
+                            p.xpoints[(k + 1)%6]-3, p.ypoints[(k + 1)%6]-5,null));
                 }
-                roads[placed++] = (new Road(p.xpoints[5]-3, p.ypoints[5]-5,
-                        p.xpoints[0]-3, p.ypoints[0]-5,null));
+              
             }
         }
     }
 
     public static boolean AlreadyIn(int x, int y)
-    {
+        // checks if a settlement is already inside settlements array, by distance
+       {
         for (int i = 0; i < settlements.length; i++) {
            if(settlements[i] == null){return false;}
            if(Math.abs(settlements[i].x - x) <20 && Math.abs(settlements[i].y - y) < 20){return true;}
@@ -323,7 +249,7 @@ public class MyPanel extends JLayeredPane implements ActionListener {
         return false;
     }
     public int determineResource(Color c)
-    {
+    { // determine resource by color
         if (c.equals(WOOL_COLOR)){return 4;}
         else if( c.equals(FOREST_COLOR)){return 1;}
         else if(c.equals(HILLS_COLOR)){return 2;}
@@ -331,33 +257,26 @@ public class MyPanel extends JLayeredPane implements ActionListener {
         else if(c.equals(GRAIN_COLOR)){return 3;};
         return -1;
     }
-    public void printTiles()
-    {
-        for (int i = 0; i < tileArray.length; i++) {
-            System.out.println("x:"+ Arrays.toString(tileArray[i].polygon.xpoints));
-            System.out.println("y:"+ Arrays.toString(tileArray[i].polygon.ypoints));
-        }
-    }
-
+   
     public Color GetColor(int x)
     {
         switch(x){
             case(1):
-                return FOREST_COLOR; // forest(lumber) // g =0
+                return FOREST_COLOR; // forest(lumber)
             case(2):
-                return HILLS_COLOR; // hills(bricks) // g = 200
+                return HILLS_COLOR; // hills(bricks) 
             case(3): // land(grain)
                 return GRAIN_COLOR;
             case(4): // fields (wool)
-                return WOOL_COLOR; // g = 255
+                return WOOL_COLOR;
             case(5): // mtn(ore)
-                return ORE_COLOR; // g =128
+                return ORE_COLOR;
 
         }
         return Color.black;
     }
     public Color generateTileType(Tile [] tiles)
-    {
+    { // generates a tile color by picking a random color from tiles array
         Random rnd = new Random();
         int x = rnd.nextInt(5);
         while (tiles[x].getAmt() == 0 && !isEmpty(tiles))
@@ -379,11 +298,7 @@ public class MyPanel extends JLayeredPane implements ActionListener {
         g2.fillPolygon(poly);
         g2.setColor(Color.BLACK);
         g2.setStroke(new BasicStroke(5));
-
         g2.drawPolygon(poly);
-
-
-
     }
 
     public boolean isEmpty(Tile[] tiles)
@@ -395,7 +310,6 @@ public class MyPanel extends JLayeredPane implements ActionListener {
     }
 
     static public Polygon makeHex(int x, int y) {
-
         int smaller = 0;
         Polygon output = new Polygon();
         output.addPoint(x , y + hexagonSide);
